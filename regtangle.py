2f494eecc52acce2a8f6767d692c0c32f8dffc2b
Kevin Zhou <kzhou@dapasoft.com>
2020-04-03 14:07:15 -0400

    convert to function

--- regtangle.py
+++ regtangle.py
@@ -1,7 +1,8 @@
-width=50
-height=6
-step=1
-print('-' * width)
-for i in range(1, height-2, step):
-    print('|' + ' '*(width-2) + '|')
-print('-' * width)
+def draw_rectangle(width, height):
+    step=1
+    print('-' * width)
+    for i in range(1, height-2, step):
+        print('|' + ' '*(width-2) + '|')
+    print('-' * width)
+
+draw_rectangle(4, 5)
