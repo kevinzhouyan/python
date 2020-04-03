79f4e138a2ae9e81352dd954dfa337d1745025b1
Kevin Zhou <kzhou@dapasoft.com>
2020-04-03 14:09:14 -0400

    convert to function

--- trangle.py
+++ trangle.py
@@ -1,12 +1,15 @@
  
-start=1
-end=5
-step=1
-for i in range (start, end+1, step):
-    print(' '*(end-i)  + '*'*(i*2-1))
+def draw_diamond(size):
+    start=1
+    end=size
+    step=1
+    for i in range (start, end+1, step):
+        print(' '*(end-i)  + '*'*(i*2-1))
  
-start=4
-end=1
-step=-1
-for i in range (start, end-1, step):
-    print(' '*(start - i + 1) + '*'*(i*2-1))
+    start=size -1
+    end=1
+    step=-1
+    for i in range (start, end-1, step):
+        print(' '*(start - i + 1) + '*'*(i*2-1))
+
+draw_diamond(5)
